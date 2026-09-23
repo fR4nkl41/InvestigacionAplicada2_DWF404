96d8b380954c6046f81a8f8b220df9c6e5e69be1
Francisco Manuel Mazariego Arteaga <161494141+fR4nkl41@users.noreply.github.com>
2026-09-22 20:37:16 -0600

    Creación del Proyecto

---
+++ control-gastos-api/src/main/java/sv/edu/udb/ControlGastosApiApplication.java
@@ -0,0 +1,13 @@
+package sv.edu.udb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlGastosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlGastosApiApplication.class, args);
+	}
+
+}
