de0d450adc4925d480415d312ccafd5517eb99ab
Christopher Adiel Alberto Guido <christopherguido09@gmail.com>
2026-09-23 21:52:43 -0600

    Update TransaccionEliminadaEvent.java

--- control-gastos-api/src/main/java/sv/edu/udb/event/TransaccionEliminadaEvent.java
+++ control-gastos-api/src/main/java/sv/edu/udb/event/TransaccionEliminadaEvent.java
@@ -1,4 +1,24 @@
+TransaccionEliminadaEvent.java:
+
 package sv.edu.udb.event;
 
-public class TransaccionEliminadaEvent {
+import lombok.Getter;
+import org.springframework.context.ApplicationEvent;
+
+import java.time.LocalDateTime;
+
+/**
+ * Evento que se publica cuando se elimina una transacción.
+ */
+@Getter
+public class TransaccionEliminadaEvent extends ApplicationEvent {
+
+    private final Long transaccionId;
+    private final LocalDateTime fechaEvento;
+
+    public TransaccionEliminadaEvent(Object source, Long transaccionId) {
+        super(source);
+        this.transaccionId = transaccionId;
+        this.fechaEvento = LocalDateTime.now();
+    }
 }
