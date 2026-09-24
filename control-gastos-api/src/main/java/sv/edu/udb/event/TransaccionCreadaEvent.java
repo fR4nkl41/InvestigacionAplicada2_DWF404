7140657ebf1030c9aa25a42e73d59251beb1fc4e
Christopher Adiel Alberto Guido <christopherguido09@gmail.com>
2026-09-23 21:44:17 -0600

    Implement TransaccionCreadaEvent for transaction events
    Added TransaccionCreadaEvent class to handle transaction creation events.

--- control-gastos-api/src/main/java/sv/edu/udb/event/TransaccionCreadaEvent.java
+++ control-gastos-api/src/main/java/sv/edu/udb/event/TransaccionCreadaEvent.java
@@ -1,4 +1,27 @@
+TransaccionCreadaEvent.java:
+
 package sv.edu.udb.event;
 
-public class TransaccionCreadaEvent {
+import lombok.Getter;
+import org.springframework.context.ApplicationEvent;
+import sv.edu.udb.model.Transaccion;
+
+import java.time.LocalDateTime;
+
+/**
+ * Evento que se publica cuando se registra una nueva transacción.
+ * Extiende ApplicationEvent para integrarse con el mecanismo
+ * de eventos internos de Spring.
+ */
+@Getter
+public class TransaccionCreadaEvent extends ApplicationEvent {
+
+    private final Transaccion transaccion;
+    private final LocalDateTime fechaEvento;
+
+    public TransaccionCreadaEvent(Object source, Transaccion transaccion) {
+        super(source);
+        this.transaccion = transaccion;
+        this.fechaEvento = LocalDateTime.now();
+    }
 }
